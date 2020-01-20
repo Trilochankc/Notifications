bfa9c119c2dfef080f3ced56d215d66d42919217
Trilochankc <49303045+Trilochankc@users.noreply.github.com>
2020-01-20 10:25:45 +0545

    picture added

--- app/src/main/java/com/trilochan/notifications/MainActivity.java
+++ app/src/main/java/com/trilochan/notifications/MainActivity.java
@@ -3,12 +3,18 @@ package com.trilochan.notifications;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btnNotific1, btnNotific2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnNotific1 = findViewById(R.id.notification1);
+        btnNotific2 = findViewById(R.id.notification2);
+
     }
 }
